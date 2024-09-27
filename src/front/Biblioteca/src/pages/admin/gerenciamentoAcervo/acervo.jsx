170ce8d16b14b0a8a96766c38b25572a7b3fe62c
dev.LuizaSampaio <142552118+luizasampaio123@users.noreply.github.com>
2024-09-27 19:25:27 -0300

    Update acervo.jsx

--- src/front/Biblioteca/src/pages/admin/gerenciamentoAcervo/acervo.jsx
+++ src/front/Biblioteca/src/pages/admin/gerenciamentoAcervo/acervo.jsx
@@ -1,7 +1,80 @@
-import style from '../gerenciamentoAcervo/acervo.module.css'
+import React from "react"; 
+import style from '../gerenciamentoAcervo/acervo.module.css';
 
-export default function Acervo (){
-    return(
-        <h1>Acervo</h1>
-    )
+export function Acervo() {
+  return (
+    <>
+      <main className='content'>
+      <div className={style.filtrospesquisa}>
+        <div className={style.pesquisa}>
+          <div className={style.conteudopesquisa}>
+            <div className={style.elemento}>
+              <input type='text' required></input>
+              <div className={style.label}>Pesquisar</div>
+            </div>
+          </div>
+        </div>
+        <div className={style.filtros}>
+            <div className={style.conteudofiltros}>
+                <div className={style.generos}>Romance</div>
+                <div className={style.generos}>Ciência</div>
+                <div className={style.generos}>Terror</div>
+                <div className={style.generos}>Classicos</div>
+                <div className={style.generos}>Ficção Cientifica</div>
+            </div>
+        </div>
+    </div>
+        <section className={style.livros}>
+            
+        <h1 className={style.tConteudo}>Coleção</h1>
+        <div className={style.content}>
+        <div className={style.livro}>
+        <img src="src/pages/admin/gerenciamentoAcervo/HarryPotter.jpg" className={style.imglivro}></img>
+        <button className={style.edit}>Editar</button>
+        </div>
+
+        <div className={style.livro}>
+        <img src="src/pages/admin/gerenciamentoAcervo/HarryPotter.jpg" className={style.imglivro}></img>
+        <button className={style.edit}>Editar</button>
+        </div>
+
+        <div className={style.livro}>
+        <img src="src/pages/admin/gerenciamentoAcervo/HarryPotter.jpg" className={style.imglivro}></img>
+        <button className={style.edit}>Editar</button>
+        </div>
+
+
+        <div className={style.livro}>
+        <img src="src/pages/admin/gerenciamentoAcervo/HarryPotter.jpg" className={style.imglivro}></img>
+        <button className={style.edit}>Editar</button>
+        </div>
+
+        <div className={style.livro}>
+        <img src="src/pages/admin/gerenciamentoAcervo/HarryPotter.jpg" className={style.imglivro}></img>
+        <button className={style.edit}>Editar</button>
+        </div>
+
+        <div className={style.livro}>
+        <img src="src/pages/admin/gerenciamentoAcervo/HarryPotter.jpg" className={style.imglivro}></img>
+        <button className={style.edit}>Editar</button>
+        </div>
+
+        <div className={style.livro}>
+        <img src="src/pages/admin/gerenciamentoAcervo/HarryPotter.jpg" className={style.imglivro}></img>
+        <button className={style.edit}>Editar</button>
+        </div>
+
+        <div className={style.livro}>
+        <img src="src/pages/admin/gerenciamentoAcervo/HarryPotter.jpg" className={style.imglivro}></img>
+        <button className={style.edit}>Editar</button>
+        </div>
+
+        </div>
+        </section>
+      </main>
+      
+    </>
+  );
 }
+
+export default Acervo;
